a84e4634be484482d2708f322d8231fadcb1517a
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-21 00:03:24 +0300

    a + b reverted

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,10 +1,6 @@
 def add(a, b):
-    if b < 0:
-        b = add(~b, 1)
     while b != 0:
         carry = a & b
         a = a ^ b
         b = carry << 1
-    if a & (1 << 31):
-        a = -add(~a, 1)
     return a
