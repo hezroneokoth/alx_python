3a526a0748bbfe49953b491d922864f8b039967d
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-20 03:20:14 +0300

    Assign random signed no to variable  when executed

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 last_digit = abs(number) % 10
-print("Last digit of", number, "is", last_digit, end="")
+suffix = ""
+
 if last_digit > 5:
-    print("and is greater than 5")
+    suffix = "and is greater than 5"
 elif last_digit == 0:
-    print("and is 0")
+    suffix = "and is 0"
 else:
-    print("and is less than 6 and not 0")
-    
+    suffix = "and is less than 6 and not 0"
+
+print("Last digit of {}, is {}, {}".format(number, last_digit, suffix))
