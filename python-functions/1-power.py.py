1dab21fb24cfb3047aee8dda512f6806accde4fc
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-20 23:16:06 +0300

    handles python functions

---
+++ python-functions/1-power.py.py
@@ -0,0 +1,11 @@
+def pow(a, b):
+    result = 1
+    if b < 0:
+        a = 1 / a
+        b = -b
+    while b:
+        if b & 1:
+            result *= a
+        a *= a
+        b >>= 1
+    return result
