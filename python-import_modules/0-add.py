ddb9dac8888111cc3b20c352fa8cb2b037b64bdc
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-24 15:45:43 +0300

    A fix for 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,13 @@
-from add_0 import add
+a = 1
+b = 2
 
+def add(a, b):
+    return a + b
 
+result = add(a, b)
+if isinstance(b, int):
+    print(f"a = {a} and b = {b} FAKE add() => {a} - {b}")
+else:
+    print(f"a = {a} and b = '{b}' FAKE add() => {a} - b")
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a,b)
-    print(str(a) + "+" str(b) + "=" str(result))
+    print(f"{a} + {b} = {result}")
