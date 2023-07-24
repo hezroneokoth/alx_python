68ebc90b4b877046fd6ec6522c74c7b658a2271c
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-24 19:08:01 +0300

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,10 @@
-a = 1
-b = 2
-
+#!/usr/bin/python3
 def add(a, b):
-    return a + b
+    return (a + b)
 
-result = add(a, b)
-if isinstance(b, int):
-    print(f"a = {a} and b = {b} FAKE add() => {a} - {b}")
-else:
-    print(f"a = {a} and b = '{b}' FAKE add() => {a} - b")
 if __name__ == "__main__":
-    print(f"{a} + {b} = {result}")
+    a = 1
+    b = 2
+    result = add(a, b)
+    
+    print("{} + {} = {}".format(a, b, result))
