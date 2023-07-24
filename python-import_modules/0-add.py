e2bdb9d4b7c30788ef8c974b766376866a977e4e
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-24 13:46:17 +0300

    Fixed an issue which executed the code when imported

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
-a = 1
-b = 2
-
 from add_0 import add
-result = add(a,b)
 
-print(f"{a} + {b} = {result}")
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a,b)
+    print(f"{a} + {b} = {result}")
