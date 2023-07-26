ddcf721a97402905b24061bf7734f72562385c62
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-26 09:27:08 +0300

    Task 3. Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
 
+def safe_print_division(a, b):
+    args:
+    a: first integer
+    b: second integer
+
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+    
+    inside result: None
