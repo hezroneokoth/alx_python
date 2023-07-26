09ee9371a8c2bea487e7d542ca50d0aa126dda4a
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-26 19:04:16 +0300

    Task 0: Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,8 +2,8 @@
 
 def no_c(my_string):
 
-    new_string = []
+    new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string.append(char)
-            return ''.join(new_string)
+            new_string += char
+            return new_string
