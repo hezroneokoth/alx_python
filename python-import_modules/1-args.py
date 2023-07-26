25cc00263412e605ac23144e9c43aa384f5e4251
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-26 14:37:04 +0300

    Task 1: Making a script dynamic!

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
     if argc == 0:
-        print("0 arguments."format(argc))
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument:"format(argc))
+        print("1 argument:")
     else:
         print("{} arguments:".format(argc))
 
