c0fe654d3da42cd2a84ad7728a2d95de1c0b2922
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-26 09:55:17 +0300

    Task 3: Integers division with debug

--- python-import_modules/3-main.py
+++ python-import_modules/3-main.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-    return result
+safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 a = 12
 b = 2
