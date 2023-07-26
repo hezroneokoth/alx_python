36af7665625a5db99413fc0e7cde6f34aa97ade9
Hezrone Okoth <hezzyokoth@gmail.com>
2023-07-26 18:59:12 +0300

    Task 0: Can you C me now?

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,8 +2,8 @@
 
 def no_c(my_string):
 
-    my_string = ''
+    new_string = []
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string += char
-            return new_string
+            new_string.append(char)
+            return ''.join(new_string)
---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+
+    A: First character
+    32: Length
+    print("Length: {:d} - First character: {}".format(length, first))
