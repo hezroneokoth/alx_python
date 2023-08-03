b0b0deab986f71e1c4460a08fe1f1df34f8415a4
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 22:56:30 +0300

    Reconciled the conflicts

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -4,13 +4,13 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    This method/function checks if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    This method/function checks if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class.
 
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; False otherwise.
+        True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class; False otherwise.
     """
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,13 +4,13 @@
 
 def inherits_from(obj, a_class):
     """
-    This method/function checks if an object is an instance of or inherited from a specified class.
+    This method/function checks  if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
     
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if the object is an instance of or inherited from a specified class; False otherwise.
+        True  if the object is an instance of a class that inherited (directly or indirectly) from the specified class; False otherwise.
     """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
