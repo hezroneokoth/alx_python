68dd9fb254abc76c691d5277dd4decead3fc7db5
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 23:40:53 +0300

    empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,10 +6,15 @@ class BaseGeometry:
     """This is an empty class BaseGeometry."""
 
     def __init__(self):
-        """
-        An __innit__ method that initializes the attribute self"""
+        """An __innit__ method that initializes the attribute self."""
         pass
 
     def __dir__(self):
         """A custom __dir__ method to override default behavior."""
         return []
+    
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
