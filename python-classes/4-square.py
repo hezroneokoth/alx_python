78a4db7983d70af6a40e941c2d50733ebdb3eb83
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 20:08:41 +0300

    Task 4: Printing a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size=0):
+        """This method initialize the Square instance with an optional size."""
+        self.size = size
+    
+    @property
+    def size(self):
+        """This getter method retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This setter method sets the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def area(self):
+        """This method calculates and returns the area of the square."""
+        return self.__size ** 2
+    
+    def my_print(self):
+        """This method prints the square with # characters."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
