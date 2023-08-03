56537c7bfce6fcc7749244e57b34dca5dce1d9ab
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 19:54:57 +0300

    Task 1: Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+class Square:
+    """This is a class that defines a square."""
+    
+    def __init__(self, size=0):
+        """This method/constructor initializes the Square instance with an optional size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
