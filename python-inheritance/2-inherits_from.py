5f5fc4c5d7ec7ab900b39df9722ab9d09b2ece48
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 22:43:34 +0300

    Harmonized some conflicts

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-"""This is a module that contains a function inherits_from"""
+"""This is a module that contains a function inherits_from."""
 
 def inherits_from(obj, a_class):
     """
-    This method/function checks if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
-
+    This method/function checks if an object is an instance of or inherited from a specified class.
+    
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; False otherwise.
+        True if the object is an instance of or inherited from a specified class; False otherwise.
     """
-    return type(obj) is a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
