0d8aa9aeaea671f541c697de7ae09699311fd3d5
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 23:57:49 +0300

    rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-"""This is a module that contains a Rectangle class that inherits from BaseGeometry."""
+"""This is a module that defines the Rectangle class."""
 
 class Rectangle(BaseGeometry):
     """A class representing a rectangle."""
 
     def __init__(self, width, height):
-        """This __innit__ method Initialize a Rectangle instance with width and height."""
+        """__innit__ method initializes a Rectangle instance with width and height."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
