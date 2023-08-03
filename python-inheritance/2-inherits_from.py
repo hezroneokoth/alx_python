9108f2044dd01e55fecbb5b471e8f6022d4f8e34
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 22:34:52 +0300

    Task 2: Only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This is a module that contains a function inherits_from"""
+
+def inherits_from(obj, a_class):
+    """
+    This method/function checks if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; False otherwise.
+    """
+    return type(obj) is a_class
