adfdfe122c1f304045bcc81177c4491d2066518c
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 19:30:52 +0300

    Task 0: Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+class Square:
+    """This is a class that defines a square."""
+    
+    def __init__(self, size):
+        """This method/constructor initializes the Square instance with a given size."""
+        self.__size = size
