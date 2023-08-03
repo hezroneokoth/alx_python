fea69f58d49971e08b879cc79123f31cde11b49c
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-03 22:43:47 +0300

    Harmonization of conflicts

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
-"""
-This is a module that contains a function is_kind_of_class.
-"""
+"""This is a module that contains a function is_kind_of_class"""
 
 def is_kind_of_class(obj, a_class):
     """
-    This method/function checks if an object is an instance of or inherited from a specified class.
-    
+    This method/function checks if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if the object is an instance of or inherited from a specified class; False otherwise.
+        True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; False otherwise.
     """
-    return issubclass(type(obj), a_class)
+    return type(obj) is a_class
