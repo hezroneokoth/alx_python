2e44c8cc135b5682f9c65a31d2da57511f645c7e
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-08 14:20:56 +0300

    Class Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,11 +2,12 @@
 """ A Base module containing the class Base."""
 
 class Base:
-    """The Base class."""
+    """ The Base class """
     __nb_objects = 0
 
-    def __innit__ (self, id=None):
+    def __init__(self, id=None):
         """An initialization contructor/method"""
+        
         if id is not None:
             self.id = id
         else:
