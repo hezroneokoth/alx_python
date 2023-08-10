e9eb9bca1a22f03e8812d9188dcb166910494cb1
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-10 23:54:21 +0300

    #ErrorCode

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -10,10 +10,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
     response = requests.get(url)
-    status_code = response.status_code
-    if status_code >= 400:
-        print("Error code:", status_code)
+
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
