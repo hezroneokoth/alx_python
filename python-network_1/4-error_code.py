b5c5784b6310c929dd11fd3b2a098fb01ad17f45
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-10 23:15:02 +0300

    Task 3: Error code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL then displays the body of the response.
+If the HTTP status code is greater than or equal to 400, it prints an error message.
+"""
+
+import requests
+import sys
+""" the import scripts"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
