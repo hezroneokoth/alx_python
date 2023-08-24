2340e7a9a59ff0d9930e31ec250cac74f54b81f6
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-24 16:06:22 +0300

    added LOWER() func to handle case-sensitive search

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script displays values in states table where name matches argument provided
+script displays values in states table where name matches arg provided
 """
 
 import sys
@@ -26,10 +26,10 @@ if __name__ == "__main__":
     # creates cursor to execute queries
     cur = conn.cursor()
 
-    # creates query using format & LIKE for case-sensitive search
-    query = ("SELECT * FROM states WHERE name COLLATE utf8_general_ci "
-             "LIKE %s ORDER BY states.id")
-    cur.execute(query, (state_name,))
+    # creates query using format & LOWER() function for case-sensitive search
+    query = ("SELECT * FROM states WHERE LOWER(name) = LOWER('{}') "
+             "ORDER BY states.id".format(state_name))
+    cur.execute(query)
 
     # fetches & prints results
     results = cur.fetchall()
