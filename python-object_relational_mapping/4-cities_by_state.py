7cf65b93f234d5e6c860ff11c70d00db2531b099
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-24 16:21:44 +0300

    Task 4: Lists cities from the database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script lists all cities from hbtn_0e_4_usa db
+"""
+
+import sys
+import MySQLdb
+
+# arguments to be passed
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # connects to server thru port 3306 on localhost
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    # creates cursor to execute queries
+    cur = conn.cursor()
+
+    # executes query
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    cur.execute(query)
+
+    # fetches & prints results
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+
+    # closes cursor & connection
+    cur.close()
+    conn.close()
