2eb6c0e159cbe2afb3863d977da1d11ed334ed9f
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-24 16:52:12 +0300

    takes state name as arg, lists all its cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-cript lists all cities from hbtn_0e_4_usa db
+Script lists all cities from hbtn_0e_4_usa db
 """
 
-# import modules
 import sys
 import MySQLdb
 
-# arguments to be passed
+# Arguments to be passed
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    # connects to server thru port 3306 on localhost
+    # Connects to server through port 3306 on localhost
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,18 +23,18 @@ if __name__ == "__main__":
         charset="utf8"
     )
 
-    # creates cursor to execute queries
+    # Creates cursor to execute queries
     cur = conn.cursor()
 
-    # executes query
-    query = "SELECT * FROM cities ORDER BY cities.id"
-    cur.execute(query)
+    # Creates query
+    query = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id"
+    cur.execute(query, (state_name,))
 
-    # fetches & prints results
+    # Fetches & prints results
     results = cur.fetchall()
     for row in results:
-        print(row)
+        print(row[0])
 
-    # closes cursor & connection
+    # Closes cursor & connection
     cur.close()
     conn.close()
