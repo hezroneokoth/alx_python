491a3e55f9cc07a9ca156cade6f4ee8c6fcc028e
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-24 13:54:12 +0300

    Task 1: Lists states with a name starting with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This script lists all states with a name starting with 'N' from hbtn_0e_0_usa db."""
+""" script lists all states with name starting with 'N' from hbtn_0e_0_usa db."""
 
 import sys
 import MySQLdb
