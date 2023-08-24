e78169633e2283a020f607dfecb30b0e86dcfe2b
Hezrone Okoth <hezzyokoth@gmail.com>
2023-08-24 16:15:13 +0300

    Task 3: Safe from MySQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+script displays all values in states table where name matches the arg provided
+(safe from MySQL injections)
+"""
+
+import sys
+import MySQLdb
+
+# arguments to be passed
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # connects to server thru port 3306 on localhost
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    # creates cursor to execute queries
+    cur = conn.cursor()
+
+    # creates query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cur.execute(query, (state_name,))
+
+    # fetches & prints results
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+
+    # closes cursor & connection
+    cur.close()
+    conn.close()
