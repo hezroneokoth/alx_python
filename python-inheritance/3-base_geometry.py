8cc6188ff66b53628fd9141edf683863f6060005
Hezrone Okoth <hezzyokoth@gmail.com>
2023-11-05 10:14:37 +0300

    Task 3: An empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
 
     def __dir__(self):
         """Override dir() to exclude __init_subclass__."""
-        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
+        return [attr for attr in dir(self) if attr != '__init_subclass__']
